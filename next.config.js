f36b0bc03ba8d64e74914e607206d31c3b029b6b
devanshdeveloper <devanshkhetwani@gmail.com>
2023-12-10 05:17:00 +0530

    fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-output: "export"
+  output: "export",
+};
 
-}
-=======
-      output: "export" }
->>>>>>> 564af35de586d5118ebfee07217c406708b2426a
-
-module.exports = nextConfig
+module.exports = nextConfig;
