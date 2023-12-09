7b5bc82a0efa99a90074eb45f5d3cd18a18ef915
devanshdeveloper <devanshkhetwani@gmail.com>
2023-12-09 20:05:13 +0530

    removed loader

--- context/AuthProvider.js
+++ context/AuthProvider.js
@@ -212,13 +212,14 @@ export default function AuthProvider({ children }) {
         }}
       >
         <Alert isOpen={isOpen} onOpenChange={onOpenChange} text={alert.text} />
-        {loader.isLoading ||
+        {children}
+        {/* {loader.isLoading ||
         !loader.isFacebookSDKLoaded ||
         !loader.isGSIClientLoaded ? (
           <ProgressLayout {...loader} />
         ) : (
           children
-        )}
+        )} */}
       </AuthContext.Provider>
     </>
   );
