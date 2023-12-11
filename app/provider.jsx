8a1baacc538aea85265e0733c7fb51669a116383
devanshdeveloper <devanshkhetwani@gmail.com>
2023-12-11 23:14:43 +0530

    added svgs and removed auth

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import AuthProvider from "@/context/AuthProvider";
+// import AuthProvider from "@/context/AuthProvider";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { msalInstance } from "@/utils/msal/msalIntance";
-import { MsalProvider } from "@azure/msal-react";
+// import { msalInstance } from "@/utils/msal/msalIntance";
+// import { MsalProvider } from "@azure/msal-react";
 import { MenuProvider } from "@/context/MenuContext";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Providers({ children }) {
   return (
